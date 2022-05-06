089df9fcd54ed5bbc35a142ca900b8eb8583658d
TechnicalRanjan <developerpriyesh29@gmail.com>
2022-05-06 13:39:29 +0530

    http-example

---
+++ http-examples.js
@@ -0,0 +1,24 @@
+const { request,  get } = require('https')
+
+
+get('https://www.google.com', (res) => {
+    res.on('data', (chunk) => {
+        console.log(`Data chunk: ${chunk}`)
+    })
+
+    res.on('end', () => {
+        console.log('No more data')
+    })
+})
+
+// const req = request('https://www.google.com', (res) => {
+//     res.on('data', (chunk) => {
+//         console.log(`Data chunk: ${chunk}`)
+//     })
+
+//     res.on('end', () => {
+//         console.log('No more data')
+//     })
+// })
+
+// req.end()
